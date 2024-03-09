0e44d2d3432ffa0b3e23d4858427bf3a5ac3ba1d
Nikshithakatta <162777730+Nikshithakatta@users.noreply.github.com>
2024-03-09 10:43:41 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,56 @@
+# Strings
+s="I am Nikshitha"
+print(type(s))
+print(s)
+# Accessing characters in a string
+print(s[2])
+print(s[:6])
+print(s[0:11:2])
+print(s[::-1])
+# Comparing Strings using operators
+# Logical operators on Strings
+new="Nikshi"
+old=""
+print(new and old)
+print(new or old)
+print(not new)
+# Formatting....
+s1="{2}{1}{0}".format("i",'am',"you")
+print(s1)
+# built-in functions and methods
+s2="Nikshitha"
+print(s2)
+print(s2.center(50))
+print(s2.center(30,'a'))
+print(max(s2))
+print(min(s2))
+print(s2.split())
+print(s2.upper())
+print(s2.lower())
+print(len(s2))
+print(s2+"Katta")
+ch="A"
+print(ord(ch))
+num=102
+print(chr(num))
+new_="nikshitha"
+sub_="nikshi"
+print(new_.count(sub_))
+print(new_.endswith(sub_,0,5))
+print(new_.startswith("ni"))
+print(new_.find(sub_))
+campus="Rkv"
+print(campus)
+print(campus.replace('Rkv','Ong'))
+camp="Rkv@123"
+print(camp.isalnum())
+print(camp.isalpha())
+print(camp.isnum())
+print(camp.isdigit())
+print(camp.islower())
+print(camp.isupper())
+print(camp.
+
+
+
+
